ade3697a32449015bcfa5ce56032b5e198fc1516
Alana Nunes <alanansilva@gmail.com>
2022-02-23 15:24:01 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -9,5 +9,46 @@ public class PrimeiroPrograma {
       /* int a = 3;
         int b = 5;
         System.out.println("Hello Word " + (a+b));*/
+
+        Livro livro = new Livro("O poder da Oração", 200);
+        System.out.println(livro);
+
+
     }
 }
+
+ class Livro {
+
+    private String nome;
+    private Integer numPaginas;
+
+     public Livro(String nome, Integer numPaginas) {
+         this.nome = nome;
+         this.numPaginas = numPaginas;
+     }
+
+     public String getNome() {
+         return nome;
+     }
+
+     public void setNome(String nome) {
+         this.nome = nome;
+     }
+
+     public Integer getNumPaginas() {
+         return numPaginas;
+     }
+
+     public void setNumPaginas(Integer numPaginas) {
+         this.numPaginas = numPaginas;
+     }
+
+     @Override
+     public String toString() {
+         return "Livro{" +
+                 "nome=" + nome +
+                 ", numPaginas=" + numPaginas +
+                 '}';
+     }
+ }
+
